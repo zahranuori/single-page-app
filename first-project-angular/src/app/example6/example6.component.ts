01c49fe4ce06fe5b0b81810dac0c38d0552255db
zahra nuori sorkhani <zahranuori.1375@gmail.com>
2022-04-30 15:08:51 +0430

    property binding & event binding in project (forms)

--- first-project-angular/src/app/example6/example6.component.ts
+++ first-project-angular/src/app/example6/example6.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import {flatMap} from "rxjs";
 
 @Component({
   selector: 'app-example6',
   templateUrl: './example6.component.html',
   styleUrls: ['./example6.component.css']
 })
-export class Example6Component implements OnInit {
+export class Example6Component {
+  userName: string ="";
+  password: string ="";
+  retypePassword: string ="";
+  isPasswordMatch: boolean = false;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  public onUsernameInputChanged (e:Event){
+    this.userName = (<HTMLInputElement>e.target).value;
   }
-
+  public onPasswordInputChanged(e:Event){
+    this.password = (<HTMLInputElement>e.target).value
+  }
+  public onRetypePasswordInputChanged(e:Event){
+    this.retypePassword = (<HTMLInputElement>e.target).value
+    this.isPasswordMatch = this.password !== this.retypePassword ? false : true;
+  }
 }
