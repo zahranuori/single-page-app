7ab47716d9caa2667fb4c1bfccb2ff22b3a81b11
zahra nuori sorkhani <zahranuori.1375@gmail.com>
2022-04-30 12:23:51 +0430

    done session 5.0

---
+++ first-project-angular/src/app/example6/example6.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-example6',
+  templateUrl: './example6.component.html',
+  styleUrls: ['./example6.component.css']
+})
+export class Example6Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
